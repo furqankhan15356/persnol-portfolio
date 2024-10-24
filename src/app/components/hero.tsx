f74412cc99a450b0139c772f04dbed31921c99a2
M.FURQANKHAN <furqan.khan15356@gmail.com>
2024-10-25 04:33:26 +0500

    updated

--- src/app/components/hero.tsx
+++ src/app/components/hero.tsx
@@ -10,7 +10,7 @@ export default function Hero() {
             </div>
             <h1 className={styles.heading1}>I AM FURQAN KHAN</h1>
             <p className={styles.skills}>
-            "As a passionate front-end developer, I specialize in crafting clean,<br /> responsive, and user-friendly interfaces using modern technologies<br /> like HTML, CSS, TypeScript, and Tailwind CSS. With a strong<br /> foundation in Next.js, I focus on creating efficient, fast, and scalable<br /> web applications. My goal is to deliver seamless user experiences through <br />thoughtful design and optimized performance, while continuously learning and <br />improving my skills with every project."
+            As a passionate front-end developer, I specialize in crafting clean,<br /> responsive, and user-friendly interfaces using modern technologies<br /> like HTML, CSS, TypeScript, and Tailwind CSS. With a strong<br /> foundation in Next.js, I focus on creating efficient, fast, and scalable<br /> web applications. My goal is to deliver seamless user experiences through <br />thoughtful design and optimized performance, while continuously learning and <br />improving my skills with every project.
             </p>
         </main>
     );
